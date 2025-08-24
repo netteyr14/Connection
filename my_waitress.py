60457f32be96718c8487d285668b85fa95b3fc2f
Isorena, Arvin Jay O. <60598107+netteyr14@users.noreply.github.com>
2025-08-24 23:24:16 +0800

    Update my_waitress.py ADDED: thread=8

--- my_waitress.py
+++ my_waitress.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     print("[INFO] Starting the server...")
     # print("@127.0.0.1:5000 or 192.168.1.7:5000")
     my_port = int(os.environ.get('PORT', 5000))
-    serve(app, host='0.0.0.0', port=my_port)
+    serve(app, host='0.0.0.0', port=my_port, thread=8)
