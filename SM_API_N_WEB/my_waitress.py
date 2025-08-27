c1b88a2bc79d08f1207d5a3873fdd830c9963bc1
Isorena, Arvin Jay O. <60598107+netteyr14@users.noreply.github.com>
2025-08-28 04:38:08 +0800

    Update my_waitress.py

--- SM_API_N_WEB/my_waitress.py
+++ SM_API_N_WEB/my_waitress.py
@@ -4,5 +4,5 @@ import os
 if __name__ == '__main__':
     print("[INFO] Starting the server...")
     # print("@127.0.0.1:5000 or 192.168.1.7:5000")
-    my_port = int(os.environ.get('PORT', 5000))
-    serve(app, host='0.0.0.0', port=my_port, threads=20)
+    #my_port = int(os.environ.get('PORT', 5000))
+    serve(app, host='0.0.0.0', port=5000, threads=20)
